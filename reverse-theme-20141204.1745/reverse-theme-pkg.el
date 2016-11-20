6cdc409164c62dbc0780d3827cd6ca0c0bf78360
taihangg <taihangg@qq.com>
2016-11-20 09:07:16 +0000

    up to sml

---
+++ reverse-theme-20141204.1745/reverse-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "reverse-theme" "20141204.1745" "Reverse theme for Emacs" 'nil :url "https://github.com/syohex/emacs-reverse-theme")
---
+++ scad-mode-20160205.1043/scad-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "scad-mode" "20160205.1043" "A major mode for editing OpenSCAD code" 'nil :url "https://raw.github.com/openscad/openscad/master/contrib/scad-mode.el" :keywords '("languages"))
