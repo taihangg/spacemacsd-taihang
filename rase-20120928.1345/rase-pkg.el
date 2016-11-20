3825c9f222ea7bb6afede739e1326566ee15a6b4
taihangg <taihangg@qq.com>
2016-11-20 09:05:52 +0000

    up to rase

---
+++ rase-20120928.1345/rase-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rase" "20120928.1345" "Run At Sun Event daemon" 'nil :url "https://github.com/m00natic/rase/" :keywords '("solar" "sunrise" "sunset" "midday" "midnight"))
