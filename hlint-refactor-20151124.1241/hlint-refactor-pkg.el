dfcb40ce7566b0e8e74a2b16d006f57e70208773
taihangg <taihangg@qq.com>
2016-11-20 08:58:50 +0000

    up to js2

---
+++ hlint-refactor-20151124.1241/hlint-refactor-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "hlint-refactor" "20151124.1241" "Apply HLint suggestions" 'nil :url "https://github.com/mpickering/hlint-refactor-mode" :keywords '("haskell" "refactor"))
