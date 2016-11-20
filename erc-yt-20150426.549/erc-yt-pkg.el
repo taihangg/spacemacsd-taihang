ba2178bbdfb2806c6213ed7d8e31e524bb596074
taihangg <taihangg@qq.com>
2016-11-20 08:52:12 +0000

    up to evil

---
+++ erc-yt-20150426.549/erc-yt-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "erc-yt" "20150426.549" "An erc module to display youtube links nicely" '((dash "2.10.0")) :keywords '("multimedia"))
---
+++ espresso-theme-20130228.2348/espresso-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "espresso-theme" "20130228.2348" "Espresso Tutti Colori port for Emacs" 'nil :url "https://github.com/dgutov/espresso-theme")
