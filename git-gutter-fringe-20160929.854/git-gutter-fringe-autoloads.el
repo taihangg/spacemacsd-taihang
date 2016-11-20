902b6e93a86810def9b1614b8f83b28d3b00bb40
taihangg <taihangg@qq.com>
2016-11-20 08:57:25 +0000

    gruvbox

---
+++ git-gutter-fringe-20160929.854/git-gutter-fringe-autoloads.el
@@ -0,0 +1,16 @@
+;;; git-gutter-fringe-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("git-gutter-fringe.el") (22577 15793 471220
+;;;;;;  500000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; git-gutter-fringe-autoloads.el ends here
---
+++ github-search-20160822.1328/github-search-autoloads.el
@@ -0,0 +1,27 @@
+;;; github-search-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "github-search" "github-search.el" (22577 15826
+;;;;;;  19082 100000))
+;;; Generated autoloads from github-search.el
+
+(autoload 'github-search-user-clone-repo "github-search" "\
+Query github using SEARCH-STRING and clone the selected repository.
+
+\(fn SEARCH-STRING)" t nil)
+
+(autoload 'github-search-clone-repo "github-search" "\
+Query github using SEARCH-STRING and clone the selected repository.
+
+\(fn SEARCH-STRING)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; github-search-autoloads.el ends here
