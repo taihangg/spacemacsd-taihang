f5e7e7cb44ab29b227734603bf0d3dca73af6471
taihangg <taihangg@qq.com>
2016-11-20 09:04:19 +0000

    up to pip

---
+++ phoenix-dark-pink-theme-20150406.2002/phoenix-dark-pink-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "phoenix-dark-pink-theme" "20150406.2002" "Port of the Sublime Text 2 theme of the same name" 'nil :url "http://github.com/j0ni/phoenix-dark-pink")
